32f6cb4f5d194d12e5504ece864d25d87c7b57bd
OPXGames <78361128+OPXGames@users.noreply.github.com>
2021-03-22 21:29:30 +0100

    Add files via upload

---
+++ Home.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Home : MonoBehaviour
+{
+
+    public string levelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //options
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public GameObject settingsWindow;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+    public void CloseSettingsWindows()
+    {
+        settingsWindow.SetActive(false);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
