c12f4464f55bc7507940bbf07deca4c69a021048
OPXGames <78361128+OPXGames@users.noreply.github.com>
2021-03-24 00:13:01 +0100

    Add files via upload

---
+++ PauseMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameisPaused = false;
+
+    public GameObject pauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Paused()
+    {
+        //activer notre menu pause / l'afficher 
+        pauseMenuUI.SetActive(true);
+
+        // arreter le temps
+        Time.timeScale = 0;
+
+        // changer le statut du jeu
+
+        gameisPaused = true ;
+        Cursor.visible = true;
+
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void Resume()
+    {
+        //activer notre menu pause / l'afficher 
+        pauseMenuUI.SetActive(false);
+
+        // arreter le temps
+        Time.timeScale = 1;
+
+        // changer le statut du jeu
+
+        gameisPaused = false;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+}
---
+++ interaction.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class interaction : MonoBehaviour
+{
+    public GameObject histoire;
+    public GameObject text;
+
+    public bool touch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        touch = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, transform.forward * 2, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 2))
+        {
+
+            Debug.Log(hit.transform.name);
+            touch = true;
+        }
+        else
+        {
+            touch = false;
+        }
+
+
+        if (touch == true)
+        {
+            text.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                text.SetActive(false);
+                histoire.SetActive(true);
+
+                
+            }
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                text.SetActive(true);
+                histoire.SetActive(false);
+
+
+            }
+
+        }
+        else
+        {
+            text.SetActive(false);
+        }
+
+
+
+
+
+
+
+    }
+}
